74bc87d25389c1903fc5df02e6319651ee6ef4a9
ZuMinnn <tranminhvu28122004@gmail.com>
2025-05-11 21:23:31 +0700

    tes3

--- library_project/settings.py
+++ library_project/settings.py
@@ -1,5 +1,6 @@
 import os
 import dj_database_url
+from pathlib import Path
 
 # Thêm vào phần ALLOWED_HOSTS
 ALLOWED_HOSTS = ['*']
@@ -17,10 +18,22 @@ STATIC_URL = '/static/'
 
 # Thêm whitenoise middleware
 MIDDLEWARE = [
-    # ...
+    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    # ...
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 # Cấu hình static files
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Cấu hình bảo mật
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True 
